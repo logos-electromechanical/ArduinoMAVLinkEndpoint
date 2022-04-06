245fe0a05607e7480a243147d2882cc8fde34cf9
Pierce Nichols <pierce@logos-electro.com>
2022-04-05 20:07:45 -0700

    Adding filtering to msg forwarding to fix issues with wings

--- src/receivers/MAVLinkForwarder.h
+++ src/receivers/MAVLinkForwarder.h
@@ -2,18 +2,39 @@
 
 #include "MAVLinkEndpoint.h"
 #include "receivers/MAVLinkRXCallback.h"
+#include <vector>
+#include <algorithm>
 
 class MAVLinkForwarder : public MAVLinkRXCallback
 {
 public:
     MAVLinkForwarder() = delete;
-    MAVLinkForwarder(MAVLinkEndpoint *in, MAVLinkEndpoint *out): 
-        MAVLinkRXCallback(0, "forwarder", in), mTXEndpoint(out) {};
+    MAVLinkForwarder(MAVLinkEndpoint *in, MAVLinkEndpoint *out, MAVLinkEndpoint *report = nullptr): 
+        MAVLinkRXCallback(0, "forwarder", in), mTXEndpoint(out), mReportEndpoint(report), mAllowList(nullptr) {};
+    MAVLinkForwarder(MAVLinkEndpoint *in, MAVLinkEndpoint *out, std::vector<uint16_t> *allow, MAVLinkEndpoint *report = nullptr): 
+        MAVLinkRXCallback(0, "forwarder", in), mTXEndpoint(out), mReportEndpoint(report), mAllowList(allow) {};
     virtual ~MAVLinkForwarder() {};
     virtual bool process(mavlink_message_t *msg, mavlink_status_t *stat)
     {
-        bool result = mTXEndpoint->transmit(msg);
-        return result;
+        if ((mAllowList == nullptr) || (std::find(mAllowList->begin(), mAllowList->end(), msg->msgid) != mAllowList->end()))
+        {
+            bool result = mTXEndpoint->transmit(msg);
+            if (mReportEndpoint) 
+            {
+                mavlink_message_t repmsg;
+                char msgbuf[50];
+                snprintf(msgbuf, 50, "Fwding msg# %lu from %d to %d; last %lu", 
+                    msg->msgid, mRXEndpoint->getSystemID(), 
+                    mTXEndpoint->getSystemID(), millis() - mLastRX);
+                mavlink_msg_statustext_pack(
+                    mRXEndpoint->getSystemID(), mReportEndpoint->getComponentID(), &repmsg,
+                    7, msgbuf, 0, 0);
+                mReportEndpoint->transmit(&repmsg);
+            }
+            mLastRX = millis();
+            return result;
+        }
+        return false;
     }
     virtual void timeout(uint32_t t) {};
     virtual bool isTimeout(uint32_t t) {return false;};
@@ -21,4 +42,6 @@ public:
     virtual uint32_t getLastRX() {return mLastRX;};
 private:
     MAVLinkEndpoint *mTXEndpoint;
+    MAVLinkEndpoint *mReportEndpoint;
+    std::vector<uint16_t> *mAllowList;
 };
